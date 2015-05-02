6df7ff9a225f7181e1335b34dd0fd1d970707b88
Jim Simon <jim@blueacorn.com>
2015-05-02 02:07:39 -0400

    Refactoring how config nodes should be laid out in config.xml.  Also adding custom naming convention to config values defined by helpers

--- app/code/local/BlueAcorn/MageJsConfig/Helper/Data.php
+++ app/code/local/BlueAcorn/MageJsConfig/Helper/Data.php
@@ -3,17 +3,17 @@ class BlueAcorn_MageJsConfig_Helper_Data extends Mage_Core_Helper_Abstract {
     public function getConfig() {
         return json_encode(
             array_merge(
-                $this->getMageConfig(),
-                $this->getCustomValues()
+                $this->getSystemValues(),
+                $this->getHelperValues()
             )
         );
     }
 
-    protected function getMageConfig() {
+    protected function getSystemValues() {
         $config = array();
-        if (Mage::getConfig()->getNode('frontend/jsconfig')) {
+        if (Mage::getConfig()->getNode('jsconfig/system')) {
             // Loop through namespaces defined in frontend/jsconfig node
-            foreach(Mage::getConfig()->getNode('frontend/jsconfig')->asArray() as $namespace=>$groups) {
+            foreach(Mage::getConfig()->getNode('jsconfig/system')->asArray() as $namespace=>$groups) {
                 // Loop through groups defined as part of namespace node
                 foreach($groups as $group=>$fields) {
                     // Loop through fields defined as part of group node
@@ -33,14 +33,6 @@ class BlueAcorn_MageJsConfig_Helper_Data extends Mage_Core_Helper_Abstract {
                             $key = $path;
                         }
 
-                        // Check if type attribute exists as part of field node
-                        // and use the attribute to convert into correct data type
-                        if(!empty($value['@']['type'])) {
-                            $filter = 'FILTER_VALIDATE_' . strtoupper($value['@']['type']);
-                            $val = constant($filter);
-                            $configValue = filter_var($configValue, $val);
-                        }
-
                         // Store the config value in array
                         $config[$key] = $configValue;
                     }
@@ -48,14 +40,17 @@ class BlueAcorn_MageJsConfig_Helper_Data extends Mage_Core_Helper_Abstract {
             }
         }
 
+        // Always include base_url as part of return values
+        $config['base_url'] = Mage::getBaseUrl();
+
         return $config;
     }
 
-    protected function getCustomValues() {
+    protected function getHelperValues() {
         $config = array();
 
-        if (Mage::getConfig()->getNode('jsconfig')) {
-            foreach(Mage::getConfig()->getNode('jsconfig')->asArray() as $namespace=>$children) {
+        if (Mage::getConfig()->getNode('jsconfig/helpers')) {
+            foreach(Mage::getConfig()->getNode('jsconfig/helpers')->asArray() as $key=>$children) {
                 $method = null;
                 $class = null;
                 foreach ($children as $node=>$value) {
@@ -69,13 +64,19 @@ class BlueAcorn_MageJsConfig_Helper_Data extends Mage_Core_Helper_Abstract {
                     }
                 }
 
+                // Check if name attribute exists as part of field node
+                // and use the attribute as the key for config array.
+                // Otherwise, just use the path as the key
+                if (!empty($children['@']['name'])) {
+                    $key = $children['@']['name'];
+                }
+
                 if ($method && $class) {
                     $class = new $class();
-                    $config[$namespace] = $class->$method();
+                    $config[$key] = $class->$method();
                 }
             }
         }
-        $config['base_url'] = Mage::getBaseUrl();
 
         return $config;
     }
